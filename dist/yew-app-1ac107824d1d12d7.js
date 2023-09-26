5a9e51e83e4c6bf7925d45e5abd0a9f5797ea535
martinschultzkristensen <m4rtinsk@protonmail.com>
2023-09-26 05:01:41 +0200

    added two new function components

--- dist/yew-app-1ac107824d1d12d7.js
+++ dist/yew-app-1ac107824d1d12d7.js
@@ -586,12 +586,12 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper2753 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 224, __wbg_adapter_16);
+    imports.wbg.__wbindgen_closure_wrapper2982 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 261, __wbg_adapter_16);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper2916 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 239, __wbg_adapter_19);
+    imports.wbg.__wbindgen_closure_wrapper3145 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 276, __wbg_adapter_19);
         return addHeapObject(ret);
     };
 
@@ -633,7 +633,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('yew-app-3dfd6f625a21f101_bg.wasm', import.meta.url);
+        input = new URL('yew-app-1ac107824d1d12d7_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
