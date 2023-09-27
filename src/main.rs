7a896faab9e8e724ac9ee254073c257ca2e66132
martinschultzkristensen <m4rtinsk@protonmail.com>
2023-09-27 09:59:40 +0200

    moved one line maybe

--- src/main.rs
+++ src/main.rs
@@ -47,32 +47,32 @@ fn videos_list(VideosListProps { videos, on_click }: &VideosListProps) -> Html {
 #[function_component(App)]
 fn app() -> Html {
 
-    // let videos = vec![
-    //     Video {
-    //         id: 1,
-    //         title: "Building and breaking things".to_string(),
-    //         speaker: "John Doe".to_string(),
-    //         url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-    //     },
-    //     Video {
-    //         id: 2,
-    //         title: "The development process".to_string(),
-    //         speaker: "Jane Smith".to_string(),
-    //         url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-    //     },
-    //     Video {
-    //         id: 3,
-    //         title: "The Web 7.0".to_string(),
-    //         speaker: "Matt Miller".to_string(),
-    //         url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-    //     },
-    //     Video {
-    //         id: 4,
-    //         title: "Mouseless development".to_string(),
-    //         speaker: "Tom Jerry".to_string(),
-    //         url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-    //     },
-    // ];
+    let videos = vec![
+        Video {
+            id: 1,
+            title: "Building and breaking things".to_string(),
+            speaker: "John Doe".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 2,
+            title: "The development process".to_string(),
+            speaker: "Jane Smith".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 3,
+            title: "The Web 7.0".to_string(),
+            speaker: "Matt Miller".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 4,
+            title: "Mouseless development".to_string(),
+            speaker: "Tom Jerry".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+    ];
 
         let videos = use_state(|| vec![]);
         {
@@ -101,8 +101,7 @@ fn app() -> Html {
              selected_video.set(Some(video))
          })
      };
-
-remove    let details = selected_video.as_ref().map(|video| html! {
+         let details = selected_video.as_ref().map(|video| html! {
          <VideoDetails video={video.clone()} />
      });
 
